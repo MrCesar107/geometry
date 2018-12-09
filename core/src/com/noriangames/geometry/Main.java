437be80502b2e9ef2db13531599edd8699a7aba2
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2018-12-09 13:16:34 -0600

    Initial commit

---
+++ core/src/com/noriangames/geometry/Main.java
@@ -0,0 +1,19 @@
+package com.noriangames.geometry;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Main extends ApplicationAdapter {
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+}
