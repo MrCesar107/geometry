3b6f3225e112eda5dbcba7788ef49b5647873678
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2018-12-09 18:27:50 -0600

    First stage of game was created

--- core/src/com/noriangames/geometry/Main.java
+++ core/src/com/noriangames/geometry/Main.java
@@ -1,19 +1,12 @@
 package com.noriangames.geometry;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class Main extends ApplicationAdapter {
+
+public class Main extends Game {
+
 	@Override
 	public void create() {
-
-	}
-
-	@Override
-	public void render() {
-		super.render();
+	    setScreen(new GameScreen(this));
 	}
 }
